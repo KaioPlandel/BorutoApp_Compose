313a09448f87a512512cdbe81b0978c54c802e5d
kaioc <kaioplandel@gmail.com>
2022-10-29 07:46:44 -0300

    create databaseConverter

--- app/src/main/java/com/plandel/borutoapp_compose/data/local/HeroDatabase.kt
+++ app/src/main/java/com/plandel/borutoapp_compose/data/local/HeroDatabase.kt
@@ -2,12 +2,14 @@ package com.plandel.borutoapp_compose.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.plandel.borutoapp_compose.data.local.dao.HeroDao
 import com.plandel.borutoapp_compose.data.local.dao.HeroRemoteKeyDao
 import com.plandel.borutoapp_compose.domain.model.Hero
 import com.plandel.borutoapp_compose.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class HeroDatabase : RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
