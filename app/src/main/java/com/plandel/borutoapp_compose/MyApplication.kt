9bfcfe0ea7718a6ae2634866ca9b1ee63288de3c
kaioc <kaioplandel@gmail.com>
2022-10-29 08:16:11 -0300

    setup Dagger

---
+++ app/src/main/java/com/plandel/borutoapp_compose/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.plandel.borutoapp_compose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
---
+++ app/src/main/java/com/plandel/borutoapp_compose/di/DatabaseModule.kt
@@ -0,0 +1,27 @@
+package com.plandel.borutoapp_compose.di
+
+import android.content.Context
+import androidx.room.Room
+import com.plandel.borutoapp_compose.data.local.HeroDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        HeroDatabase::class.java,
+        "hero_database",
+    ).build()
+
+}
