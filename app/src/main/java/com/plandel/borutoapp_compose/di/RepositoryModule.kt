99f4b2645daa32caf727fcfe54825463cace067e
kaioc <kaioplandel@gmail.com>
2022-11-10 20:36:05 -0300

    setup DataStore

---
+++ app/src/main/java/com/plandel/borutoapp_compose/di/RepositoryModule.kt
@@ -0,0 +1,25 @@
+package com.plandel.borutoapp_compose.di
+
+import android.content.Context
+import com.plandel.borutoapp_compose.data.pref.DataStoreOperationsImpl
+import com.plandel.borutoapp_compose.domain.repository.DataStoreOperations
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun providesDataStoreOperations(
+        @ApplicationContext context: Context
+    ): DataStoreOperations {
+        return DataStoreOperationsImpl(context = context)
+    }
+
+}
---
+++ app/src/main/java/com/plandel/borutoapp_compose/domain/repository/DataStoreOperations.kt
@@ -0,0 +1,8 @@
+package com.plandel.borutoapp_compose.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+
+interface DataStoreOperations {
+    suspend fun saveOnBoardingState(completed: Boolean)
+    fun readOnBoardingState(): Flow<Boolean>
+}
