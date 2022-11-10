282543a523a1a257fe19ea1a534a9b27ad66d17b
kaioc <kaioplandel@gmail.com>
2022-11-10 20:36:38 -0300

    add animation

--- app/src/main/java/com/plandel/borutoapp_compose/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/plandel/borutoapp_compose/presentation/splash/SplashScreen.kt
@@ -1,13 +1,18 @@
 package com.plandel.borutoapp_compose.presentation.splash
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -19,11 +24,26 @@ import com.plandel.borutoapp_compose.ui.theme.Purple700
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    Splash()
+
+    val degrees = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        degrees.animateTo(
+            targetValue = 360f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                delayMillis = 200
+            )
+        )
+    }
+
+    Splash(degrees = degrees.value)
 }
 
 @Composable
-fun Splash() {
+fun Splash(degrees: Float) {
     if (isSystemInDarkTheme()) {
         Box(
             modifier = Modifier
@@ -32,6 +52,7 @@ fun Splash() {
             contentAlignment = Alignment.Center
         ) {
             Image(
+                modifier = Modifier.rotate(degrees = degrees),
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = stringResource(R.string.app_logo)
             )
@@ -51,6 +72,7 @@ fun Splash() {
             contentAlignment = Alignment.Center
         ) {
             Image(
+                modifier = Modifier.rotate(degrees = degrees),
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = stringResource(R.string.app_logo)
             )
